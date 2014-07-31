69102b0bbd96a9c55e54a57b026d0aea4a008670
tcain <tcain@weblinc.com>
2014-07-31 11:26:44 -0400

    change parse_folder method to return untracked and modified files only

--- lib/cf_migrate.rb
+++ lib/cf_migrate.rb
@@ -1,14 +1,10 @@
 require "cf_migrate/version"
 
 module CfMigrate
-  def build
-
-  end
-
   def check
-    sprocs = parse_folder("db/sprocs")
-    functions = parse_folder("db/functions")
-    views = parse_folder("db/views")
+    sprocs = parse_folder("db/sprocs/")
+    functions = parse_folder("db/functions/")
+    views = parse_folder("db/views/")
     
     puts "Sprocs to Build: #{sprocs}"
     puts "Functions to Build: #{functions}"
@@ -18,9 +14,9 @@ module CfMigrate
   private 
 
   def parse_folder(folder)
-  	tmp = Dir.glob("#{folder}/**").sort_by {|f| File.mtime(f)}.reverse
-    tmp.reject {|f| File.mtime(f).strftime("%F") != Time.now.strftime("%F")}
+  	tmp = chdir(folder)
+    files = `git ls-files --others --modified | grep '\.sql$'`.split($/)
 
-    return tmp
+    return files
   end
 end
