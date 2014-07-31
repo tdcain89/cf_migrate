476b4e4bbfe842c83fbc2b3e6c827938e7161d81
tcain <tcain@weblinc.com>
2014-07-31 14:04:57 -0400

    begin building out allObjects script

--- lib/cf_migrate.rb
+++ lib/cf_migrate.rb
@@ -1,12 +1,28 @@
 require "cf_migrate/version"
 
+ROOT_DIR = Dir.pwd
+
 module CfMigrate
   class Migrate
-    def check
-      tmp = Dir.chdir("db/")
-      sprocs = parse_folder("sprocs/")
-    
-      puts "Sprocs to Build: #{sprocs}"
+    def build_all_objects
+      sprocs = get_sprocs
+      
+      sprocs.each do |sproc|
+        tmp_content = File.Open(sproc, 'r')
+        File.open ("allObjects.sql", 'w') {|f| f.write(tmp_content) }
+      end
+    end
+
+    def get_sprocs
+      sprocs = parse_folder("db/sprocs/")
+    end
+
+    def get_functions
+      functions = parse_folder("db/functions/")
+    end
+
+    def get_views
+      views = parse_folder("db/views/")
     end
 
     private 
@@ -14,7 +30,8 @@ module CfMigrate
     def parse_folder(folder)
   	  tmp = Dir.chdir(folder)
       files = `git ls-files --others --modified | grep '\.sql$'`.split($/)
-
+      tmp = Dir.chdir(ROOT_DIR)
+      
       return files
     end
   end
